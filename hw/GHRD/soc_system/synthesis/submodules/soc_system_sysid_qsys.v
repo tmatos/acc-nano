868461502c23215dabdc76ebef13a3665140c6b0
Tiago Matos <tiagoms88@gmail.com>
2019-08-30 21:33:42 -0300

    generate soc system for working atax hw

--- hw/GHRD/soc_system/synthesis/submodules/soc_system_sysid_qsys.v
+++ hw/GHRD/soc_system/synthesis/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1566493161 : 2899645186;
+  assign readdata = address ? 1566503542 : 2899645186;
 
 endmodule
 
